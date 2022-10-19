0dd83af4251b9f5fe97f9864bbca873bdb8550b8
Arjun Verma <verma212174@gmail.com>
2022-10-19 14:28:52 +0530

    deackted palindrome

---
+++ git_practice_asssi.js
@@ -0,0 +1,11 @@
+ var str="arjun";
+ var N=5;
+
+ var tem="";
+  for(var i=N-1; i>=0; i--){
+      tem +=str[i];
+  }if(str===tem){
+      console.log("Yes");
+  }else{
+      console.log("No")
+  }
